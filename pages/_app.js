f294cef261a3a57d09555eae76d06631c06329ab
Vadx <vadim.lysenko@solidopinion.com>
2020-05-12 12:49:38 +0300

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,6 @@ export default function App (props) {
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Nav />
         <Container>
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,36 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
+import Link from 'next/link'
+import fs from 'fs'
 
-export default function Index () {
+export default function Index ({ slugs }) {
   return (
     <>
       <Box my={4}>
         <Typography variant='h4' component='h1' gutterBottom>
-          Next.js example
+          Slugs:
         </Typography>
+        {slugs.map(slug => {
+          return (
+            <div key={slug}>
+              <Link href='/blog/[slug]' as={`/blog/${slug}`}>
+                {/* <a>{`/blog/${slug}`}</a> */}
+                <Typography>{slug}</Typography>
+              </Link>
+            </div>
+          )
+        })}
       </Box>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync('posts')
+  return {
+    props: {
+      slugs: files.map(filename => filename.replace('.md', ''))
+    }
+  }
 }
